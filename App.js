60f0a4d0d82809d71bc50b87c10a1dd45d5a515d
leahzeisner <leahzeisner@gmail.com>
2024-03-19 11:36:16 -0400

    a5 lab up to 3.5

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from './labs/Lab5.js'
 const app = express()
+app.use(cors());
 Lab5(app);
 Hello(app)
 app.listen(4000)
