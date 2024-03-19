5f5b910033bb82c9b0ff113c6545f2cb5e9bd5f7
leahzeisner <leahzeisner@gmail.com>
2024-03-19 19:18:45 -0400

    assignment routes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Hello from "./Hello.js"
 import Lab5 from './labs/Lab5.js'
 import CourseRoutes from './Kanbas/courses/routes.js'
 import ModuleRoutes from './Kanbas/modules/routes.js'
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 const app = express()
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,5 @@ Lab5(app);
 Hello(app)
 CourseRoutes(app)
 ModuleRoutes(app)
+AssignmentRoutes(app)
 app.listen(4000)
---
+++ Kanbas/assignments/routes.js
@@ -0,0 +1,11 @@
+import db from "../Database/index.js";
+function AssignmentRoutes(app) {
+    app.get("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const assignments = db.assignments
+            .filter((m) => m.courseId === cid);
+        res.send(assignments);
+    });
+}
+
+export default AssignmentRoutes;
