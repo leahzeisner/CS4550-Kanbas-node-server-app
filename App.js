22ecc94429ee9de01571905222a2643c9ece3bde
leahzeisner <leahzeisner@gmail.com>
2024-04-01 18:11:07 -0400

    signin/signup

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express from 'express'
 import mongoose from "mongoose";
+import session from "express-session";
 import cors from "cors";
 import Hello from "./Hello.js"
 import Lab5 from './labs/Lab5.js'
@@ -12,7 +14,27 @@ import ComingUpRoutes from './Kanbas/comingups/routes.js';
 
 mongoose.connect("mongodb://localhost:27017/kanbas?directConnection=true");
 const app = express()
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 Lab5(app);
 Hello(app)
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,7 +1,8 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+
 export default function UserRoutes(app) {
   const createUser = async (req, res) => {
+    delete req.body._id;
     const user = await dao.createUser(req.body);
     res.json(user);
   };
@@ -31,35 +32,46 @@ export default function UserRoutes(app) {
   const updateUser = async (req, res) => {
     const { userId } = req.params;
     const status = await dao.updateUser(userId, req.body);
-    currentUser = await dao.findUserById(userId);
+    const currentUser = await dao.findUserById(userId);
+    req.session["currentUser"] = currentUser;
     res.json(status);
   };
 
   const signup = async (req, res) => {
-    console.log(req.body.username)
     const user = await dao.findUserByUsername(req.body.username);
     if (user) {
-      console.log('user exists')
       res.status(400).json(
         { message: "Username already taken" });
       return
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json(
+        { message: "Username or password not found" });
+    }
   };
 
   const signout = (req, res) => {
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
 
   const profile = async (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
 
