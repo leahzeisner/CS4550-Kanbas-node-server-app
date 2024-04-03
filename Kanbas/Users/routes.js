87b047a9c5014f5a8d4cf35eefbfc2c966b2df59
leahzeisner <leahzeisner@gmail.com>
2024-04-03 12:36:45 -0400

    only update current user session if updating current user

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -30,10 +30,12 @@ export default function UserRoutes(app) {
   };
 
   const updateUser = async (req, res) => {
-    const { userId } = req.params;
-    const status = await dao.updateUser(userId, req.body);
-    const currentUser = await dao.findUserById(userId);
-    req.session["currentUser"] = currentUser;
+    const { user, isCurrentUser } = req.body;
+    const status = await dao.updateUser(user._id, user);
+    if (isCurrentUser) {
+      const currentUser = await dao.findUserById(user._id);
+      req.session["currentUser"] = currentUser;
+    }
     res.json(status);
   };
 
